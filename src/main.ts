f89ec674cdb0aa7eaab266721d2cc50f0ab39d92
WannJJ <76657794+WannJJ@users.noreply.github.com>
2026-07-14 20:23:28 +0200

    feat: add JwtStrategy

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,22 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000', // FE port
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
     credentials: true,
   });
 
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.use(cookieParser());
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
-  await app.listen(3001);
+  await app.listen(process.env.PORT || 3001);
+  console.log(
+    `🚀 Server running on http://localhost:${process.env.PORT || 3001}`,
+  );
 }
 bootstrap();
