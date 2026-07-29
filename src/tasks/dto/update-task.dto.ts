b77087cba249eac28d8508fae3edae4c53b1a7b9
WannJJ <76657794+WannJJ@users.noreply.github.com>
2026-07-29 22:51:36 +0200

    chore: remove incomplete tasks module to unblock server and focus on auth

--- src/tasks/dto/update-task.dto.ts
+++
@@ -1,5 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateTaskDto } from './create-task.dto';
-
-// PartialType: tất cả field đều optional, kế thừa validation từ CreateTaskDto
-export class UpdateTaskDto extends PartialType(CreateTaskDto) {}
